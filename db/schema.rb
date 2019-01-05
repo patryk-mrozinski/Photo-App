79869b6a058359395b5c4aca57c128362808f6c8
Patryk <patryk1m1@gmail.com>
2019-01-06 00:35:04 +0100

    Start of payment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_02_203540) do
+ActiveRecord::Schema.define(version: 2019_01_05_230537) do
+
+  create_table "payments", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
